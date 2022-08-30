c9f24f00866f6525ffbb995bbd3aea62a680f31a
Donalson <donalson1517@gmail.com>
2022-08-30 15:59:35 -0600

    Creacion de la Apps
    Se incorporo la app de login y de evento poesia al proyecto

--- Poesia/Poesia/urls.py
+++ Poesia/Poesia/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -22,4 +22,6 @@ urlpatterns = [
 
     path('', views.index, name='Index'),
     path('reportes_poesia/', views.reportes_poesia, name='Reportes_Poesia'),
+
+    path('', include('EventoPoesia.urls')),
 ]
